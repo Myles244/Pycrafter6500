fe11da7e19b3ee81d7fab8d13ff57af4cf81468b
Myles244 <43722484+Myles244@users.noreply.github.com>
2025-11-27 13:58:59 +0000

    made installable packacge

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pycrafter6500',
+    version='0.1.0',
+    description='Local editable version of Pycrafter6500',
+    # This tells pip to include the specific .py files found in the root directory
+    py_modules=['pycrafter6500', 'erle'], 
+    install_requires=[
+        # If the library relies on other packages (like pyserial or pillow), 
+        # add them here, e.g.: 'pyserial', 'Pillow'
+    ],
+)
